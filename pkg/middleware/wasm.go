efd6716088068c7a3b5022ca6d6f76e01d2012d6
Jacob <jmccord.dev@gmail.com>
2026-05-27 18:21:48 -0400

    feat: added wazero dependency and base WasmMiddleware struct

---
+++ pkg/middleware/wasm.go
@@ -0,0 +1,52 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"os"
+
+	"github.com/tetratelabs/wazero"
+)
+
+type WasmMiddleware struct {
+	runtime wazero.Runtime
+	module  wazero.CompiledModule
+	next    http.Handler
+}
+
+func NewWasmMiddleware(ctx context.Context, wasmPath string, next http.Handler) (*WasmMiddleware, error) {
+	r := wazero.NewRuntime(ctx)
+
+	wasmBytes, err := os.ReadFile(wasmPath)
+	if err != nil {
+		return nil, err
+	}
+
+	compiled, err := r.CompileModule(ctx, wasmBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return &WasmMiddleware{
+		runtime: r,
+		module:  compiled,
+		next:    next,
+	}, nil
+}
+
+func (m *WasmMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	mod, err := m.runtime.InstantiateModule(ctx, m.module, wazero.NewModuleConfig().WithName("plugin"))
+	if err == nil {
+		fn := mod.ExportedFunction("process_request")
+		if fn != nil {
+			_, _ = fn.Call(ctx)
+		}
+		mod.Close(ctx)
+	}
+
+	if m.next != nil {
+		m.next.ServeHTTP(w, r)
+	}
+}
